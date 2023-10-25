9a2046721ee28e2c0ddcbbceca3814ab17cc3f8a
xxxupnodown <469004811@qq.com>
2023-10-25 22:11:27 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
  # @Author: wanghaoyu
  # @Date: 2023-09-27 21:31:18
  # @LastEditors: wanghaoyu
- # @LastEditTime: 2023-10-25 22:10:17
+ # @LastEditTime: 2023-10-25 22:11:25
 ### 
 # 移动所有文件到 nginx html 中
-cp ./* /usr/local/nginx/html
+cp -r ./* /usr/local/nginx/html
