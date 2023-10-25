95dc989bc61e1f4b6131a9214959731f4503c9f9
xxxupnodown <469004811@qq.com>
2023-10-25 22:10:31 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 # 部署目录###
+###
  # @Description: file content
  # @Author: wanghaoyu
  # @Date: 2023-09-27 21:31:18
  # @LastEditors: wanghaoyu
- # @LastEditTime: 2023-09-27 21:31:29
+ # @LastEditTime: 2023-10-25 22:10:17
 ### 
 # 移动所有文件到 nginx html 中
-mv ./* /usr/local/nginx/html
+cp ./* /usr/local/nginx/html
